cec6b7af459e8b946b0d61887d42e6700e0d11e9
JovemDevv <anacarolinesan18@gmail.com>
2023-08-16 14:30:53 -0300

    Construindo formulario e adicionando as atividades

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,19 +1,91 @@
-
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [atividades, setAtividades] = useState([]);
+
+  const [novaAtividade, setNovaAtividade] = useState({ id: '', descricao: '' });
+
+  function addAtividade(e) {
+    e.preventDefault();
+
+    const novoId = atividades.length > 0 ? atividades[atividades.length - 1].id + 1 : 1;
+
+    const atividade = {
+      id: novoId,
+      descricao: novaAtividade.descricao
+    };
+
+    setAtividades([...atividades, { ...atividade}]);
+
+    // Limpar campos
+    setNovaAtividade({ id: '', descricao: '' });
+    console.log(atividades)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <ul>
-          <li>1 Atividade</li>
-          <li>2 Atividade</li>
-          <li>3 Atividade</li>
-          <li>4 Atividade</li>
-        </ul>
-      </header>
+    <div>
+      <form className="row g-3">
+      <div className="form-group col-md-5">
+        <label htmlFor="inputEmail4">ID</label>
+        <input id="id"
+        type="text"  
+        className="form-control" 
+        placeholder="ID"
+            value={novaAtividade.id}
+            onChange={(e) => setNovaAtividade({ ...novaAtividade, id: e.target.value })} />
+      </div>
+
+      <div className="form-group col-md-5">
+        <label htmlFor="inputEmail4">Descrição</label>
+        <input
+              id="descricao"
+              type="text"
+              className="form-control" 
+              placeholder="Descrição"
+              value={novaAtividade.descricao}
+              onChange={(e) => 
+              setNovaAtividade({ ...novaAtividade, 
+                descricao: e.target.value })}
+            />
+      </div>
+        <br />
+        <div className='col-12'>
+          <button 
+            className="btn btn-outline-dark"
+            onClick={addAtividade}>
+              + Atividade
+          </button>
+        </div>
+      </form>
+      <br />
+      <div className="nt-3">
+        
+          {atividades.map((ativ) => (
+            //style={{width: "18rem"}}
+            <div key={ativ.id} class="card mb-2 shadow-sm" >              
+              <div className="card-body">
+                <div className='d-flex justify-content-between'>
+                  <h5 className='card-title'>
+                  <span  className="badge  badge-dark me-2">
+            
+                      {ativ.id}
+                      </span>
+                      - título
+                  </h5>
+                  <h6>
+                    Prioridade: Normal
+                  </h6>
+                </div>
+                <p className="card-text">{ativ.id} - {ativ.descricao}
+                </p>
+            </div>
+          </div>
+          ))}
+        
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- crud/src/index.js
+++ crud/src/index.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
 
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-
-
-ReactDOM.render(
-  <>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
+  <div className="container">
     <App />
-  </>,
-  document.getElementById('root')
-)
-
+  </div>
+);
