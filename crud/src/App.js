032a28179378e4748b53973d4ba7386e74a26e69
JovemDevv <anacarolinesan18@gmail.com>
2023-08-16 11:46:24 -0300

    create

---
+++ crud/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>1 Atividade</li>
+          <li>2 Atividade</li>
+          <li>3 Atividade</li>
+          <li>4 Atividade</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App
