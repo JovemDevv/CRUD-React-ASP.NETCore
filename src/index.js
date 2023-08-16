4032d5e41d6ad7ddcb08484f805f890a5418faed
JovemDevv <anacarolinesan18@gmail.com>
2023-08-16 10:21:38 -0300

    Criando o Readme

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App'
 import reporWebVitals from './reportWebVitals' // Importe o componente principal do seu aplicativo (você pode nomeá-lo como quiser)
 
 ReactDOM.render(
-    <Fragment>
+    <>
         <App />
-    </Fragment>,
+    </>,
  document.getElementById('root')
  )
 
